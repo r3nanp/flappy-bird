21b2aa8671ec0e62078ed5aa462a529e05f0523f
r3nanp <renanmol87@gmail.com>
2020-11-21 18:58:35 -0300

    feat (script): change the name to script and remove unused code

---
+++ src/script.js
@@ -0,0 +1,17 @@
+import main from './scripts/index.js'
+
+window.addEventListener('click', () => {
+  if (main.displayActive.click || main.displayActive.over) {
+    main.displayActive.click()
+  }
+})
+
+window.addEventListener('keydown', event => {
+  if (main.displayActive.click && !event.repeat && event.code === 'Space') {
+    main.displayActive.click()
+  }
+})
+
+main.changeDisplay(main.display.key)
+
+main.renderScreen()
