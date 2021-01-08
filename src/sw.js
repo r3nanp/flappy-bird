8fbc7d0582cf307533835a1e1405beb88a519dc1
r3nanp <renanmol87@gmail.com>
2021-01-08 17:15:15 -0300

    feat(sw): create a service worker

---
+++ src/sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'Flappy Bird'
+
+// Cache all the files to make a PWA
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      return cache.addAll([
+        '../index.html',
+        '../public/manifest.json',
+        '../effects/hitsound.mp3',
+      ])
+    })
+  )
+})
+
+// Our service worker will intercept all fetch requests
+// and check if we have cached the file
+// if so it will serve the cached file
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request)
+      })
+  )
+})
