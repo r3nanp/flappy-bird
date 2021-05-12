c6fbdc632c9de4cf5f9ae528f0b3f964308b7108
r3nanp <renanmol87@gmail.com>
2021-05-12 15:43:10 -0300

    feat: start the game when dom load

--- src/game.js
+++ src/game.js
@@ -2,6 +2,13 @@ import main from './scripts/index.js'
 
 const canvas = document.querySelector('canvas')
 
+window.addEventListener('DOMContentLoaded', () => {
+  main.changeScreen(main.display.start)
+
+  main.renderScreen()
+})
+
+
 canvas.addEventListener('mousedown', () => {
   if (main.screenActive.click || main.screenActive.over) {
     main.screenActive.click()
@@ -16,7 +23,3 @@ window.addEventListener('keydown', event => {
     main.screenActive.click()
   }
 })
-
-main.changeScreen(main.display.start)
-
-main.renderScreen()
