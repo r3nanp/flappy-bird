ac5a1b89c540b046108f65b92874baae8ef8db36
r3nanp <renanmol87@gmail.com>
2021-05-12 15:37:26 -0300

    refactor: change to a descriptive name

---
+++ src/scripts/createScreen.js
@@ -0,0 +1,12 @@
+export default function (main) {
+  main.changeScreen = newScreen => {
+    main.screenActive = newScreen
+
+    if (main.screenActive.started) {
+      main.screenActive.started()
+    }
+    if (main.screenActive.over) {
+      main.screenActive.over()
+    }
+  }
+}
