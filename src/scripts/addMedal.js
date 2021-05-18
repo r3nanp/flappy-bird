0f315433551f914215e7846929b048300b8dbae4
r3nanp <renanmol87@gmail.com>
2021-05-18 09:13:14 -0300

    refactor: add functions

--- src/scripts/addMedal.js
+++ src/scripts/addMedal.js
@@ -1,57 +1,20 @@
 import { score } from './createScore.js'
-import { goldMedal, goldenMedal, silverMedal, whiteMedal } from './medals.js'
+import { drawMedals } from './medals.js'
 
 export default function (main) {
+  const { drawWhiteMedal, drawSilverMedal, drawGoldMedal, drawGoldenMedal } =
+    drawMedals
+
   main.addMedal = {
     draw() {
       if (score <= 10) {
-        main.context.drawImage(
-          main.sprites,
-          whiteMedal.initialX,
-          whiteMedal.initialY,
-          whiteMedal.width,
-          whiteMedal.height,
-          whiteMedal.canvasX,
-          whiteMedal.canvasY,
-          whiteMedal.width,
-          whiteMedal.height
-        )
+        drawWhiteMedal(main)
       } else if (score <= 20) {
-        main.context.drawImage(
-          main.sprites,
-          silverMedal.finalX,
-          silverMedal.initialY,
-          silverMedal.width,
-          silverMedal.height,
-          silverMedal.canvasX,
-          silverMedal.canvasY,
-          silverMedal.width,
-          silverMedal.height
-        )
+        drawSilverMedal(main)
       } else if (score <= 30) {
-        main.context.drawImage(
-          main.sprites,
-          goldMedal.initialX,
-          goldMedal.finalY,
-          goldMedal.width,
-          goldMedal.height,
-          goldMedal.canvasX,
-          goldMedal.canvasY,
-          goldMedal.width,
-          goldMedal.height
-        )
+        drawGoldMedal(main)
       } else if (score > 30) {
-        main.context.drawImage(
-          main.sprites,
-          goldenMedal.finalX,
-          goldenMedal.finalY,
-          goldenMedal.width,
-          goldenMedal.height,
-          goldenMedal.canvasX,
-          goldenMedal.canvasY,
-          goldenMedal.width,
-          goldenMedal.height
-        )
+        drawGoldenMedal(main)
       }
     },
     update() {
