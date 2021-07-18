0a053302dad13f1fdb97bdc9c57c30b691c79870
Renan Pereira <renanmol87@gmail.com>
2021-07-18 20:08:52 -0300

    feat: get a medal based on a score.

--- src/scripts/addMedal.js
+++ src/scripts/addMedal.js
@@ -3,30 +3,34 @@ import { drawMedals } from './medals.js'
 
 /**
  * Return a function that draws a medal to a user's score.
- * @param {object} main
+ * @param {object} main - Main object
  */
 
 export default function (main) {
-  const { drawSilverMedal, drawGoldMedal, drawBronzeMedal } = drawMedals
+  const { drawSilverMedal, drawGoldMedal, drawBronzeMedal, drawWhiteMedal } =
+    drawMedals
 
   main.addMedal = {
     draw() {
-      const medal = score >= 30 ? 'gold' : score >= 20 ? 'silver' : 'bronze'
+      const medal = score > 30 ? 'gold' : score <= 15 ? 'bronze' : 'silver'
 
       /**
        * Get a medal based on a score.
        * @param {String} medal - Medals to draw
        * @param {object} main - Main object
-       */
+      */
 
       const getMedal = (medal, main) => {
-        const medals = {
-          bronze: drawBronzeMedal(main),
-          silver: drawSilverMedal(main),
-          gold: drawGoldMedal(main),
+        switch (medal) {
+          case 'silver':
+            return drawSilverMedal(main)
+          case 'gold':
+            return drawGoldMedal(main)
+          case 'bronze':
+            return drawBronzeMedal(main)
+          default:
+            return drawWhiteMedal(main)
         }
-
-        return medals[medal]
       }
 
       getMedal(medal, main)
