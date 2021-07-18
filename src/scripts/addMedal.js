4ee18e892c5519ae2b35d6e3a561d0142284df5b
Renan Pereira <renanmol87@gmail.com>
2021-07-18 20:05:37 -0300

    Revert "feat(add medal): create function to add medals"

--- src/scripts/addMedal.js
+++ src/scripts/addMedal.js
@@ -1,35 +1,21 @@
 import { score } from './createScore.js'
 import { drawMedals } from './medals.js'
 
-/**
- * Return a function that draws a medal to a user's score.
- * @param {object} main
- */
-
 export default function (main) {
-  const { drawSilverMedal, drawGoldMedal, drawBronzeMedal } = drawMedals
+  const { drawWhiteMedal, drawSilverMedal, drawGoldMedal, drawGoldenMedal } =
+    drawMedals
 
   main.addMedal = {
     draw() {
-      const medal = score >= 30 ? 'gold' : score >= 20 ? 'silver' : 'bronze'
-
-      /**
-       * Get a medal based on a score.
-       * @param {String} medal - Medals to draw
-       * @param {object} main - Main object
-       */
-
-      const getMedal = (medal, main) => {
-        const medals = {
-          bronze: drawBronzeMedal(main),
-          silver: drawSilverMedal(main),
-          gold: drawGoldMedal(main),
-        }
-
-        return medals[medal]
+      if (score <= 10) {
+        drawWhiteMedal(main)
+      } else if (score <= 20) {
+        drawSilverMedal(main)
+      } else if (score <= 30) {
+        drawGoldMedal(main)
+      } else if (score > 30) {
+        drawGoldenMedal(main)
       }
-
-      getMedal(medal, main)
     },
     update() {
       this.draw()
